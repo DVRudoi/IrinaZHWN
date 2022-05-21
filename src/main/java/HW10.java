7982a98889fbefa6dddb7561c18f9bc2b976e397
DVRudoi <dvrudoy@gmail.com>
2022-05-21 13:56:08 +0300

    HW10 1-5;

--- src/main/java/HW10.java
+++ src/main/java/HW10.java
@@ -1,2 +1,56 @@
 public class HW10 {
+    //1.
+    public String task1(String a){
+        if (!a.isEmpty()){
+            String str = a.trim();
+            if (str.length() == a.length()){
+                return "Пробелов не было";
+            } else {
+                return "Лишние пробелы были удалены";
+            }
+        } else {
+            return "Строка пустая";
+        }
+    }
+
+    //2.
+    public String RemoveAlla(String a){
+        if (!a.isEmpty()){
+            return a.trim().replace("a","");
+        } else {
+            return "Строка пустая";
+        }
+    }
+
+    //3.
+    public String taskRemoveAllZeros(String a){
+        if (!a.isEmpty()){
+            return a.trim().replace(" ","").replace("0","");
+        } else {
+            return "Строка пустая";
+        }
+    }
+
+    //4.
+    public String RemoveAllSpaces(String a){
+        if (!a.isEmpty()){
+            return a.trim().replace(" ","");
+        } else {
+            return "Строка пустая";
+        }
+    }
+
+    //5.
+    public int RemoveLength(String a){
+        if (!a.isEmpty()){
+            a.trim();
+            String str = a.toLowerCase().replace("a","");
+            return a.length() - str.length();
+        } else {
+            return 0;
+        }
+    }
+
+    //6.
+
 }
---
+++ src/main/java/HW10Main.java
@@ -0,0 +1,6 @@
+public class HW10Main {
+    public static void main(String[] args) {
+        HW10 hw10 = new HW10();
+        System.out.println(hw10.task1(""));
+    }
+}
