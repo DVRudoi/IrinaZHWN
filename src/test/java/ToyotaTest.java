b206e01097ff64943574dd714bc5b73a2682f43c
DVRudoi <dvrudoy@gmail.com>
2022-05-25 19:15:31 +0300

    ToyotaTest

--- src/test/java/ToyotaTest.java
+++ src/test/java/ToyotaTest.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class ToyotaTest {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Ignore;
+import org.testng.annotations.Test;
+import runner.BaseTest;
+
+public class ToyotaTest extends BaseTest {
+
+    @Test
+    public void testDmitriyRudoi() throws InterruptedException {
+        getDriver().get("https://www.toyota.com/");
+
+        WebElement searchButton = getDriver().findElement(
+                By.xpath("//button[@class='search']"));    //"//*[@id='tcom-header']/div[2]/nav/ul/li[4]/button"));
+        Thread.sleep(500);
+        searchButton.click();
+
+        WebElement searchBox = getDriver().findElement(By.name("keyword"));
+        searchBox.sendKeys("2022 Corolla");
+
+        WebElement goButton = getDriver().findElement(
+                By.xpath(  "//button[@aria-label='Search submit']"));   //"//*[@id='tcom-header']/div[3]/div/div[2]/div/div[2]/div/form/button"));
+        goButton.click();
+
+        WebElement toyotaButton = getDriver().findElement(
+                By.xpath("//a[@class='logo']"));    //"//*[@id='tcom-header']/div[2]/nav/ul/li[1]/a[1]"));
+        toyotaButton.click();
+
+        String expectedExploreAllVehicles = getDriver()
+                .findElement(By.xpath("//h2[text()='Explore All Vehicles']"))
+                .getText();                                                                       //   "/html/body/div[1]/div/div[2]/div/section[1]/div/div/div[1]/div[1]/h2"));
+
+        Assert.assertEquals(expectedExploreAllVehicles, "Explore All Vehicles");
+    }
+
 }
--- src/test/java/runner/BaseTest.java
+++ src/test/java/runner/BaseTest.java
@@ -1,2 +1,23 @@
-package runner;public class BaseTest {
+package runner;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.*;
+
+public abstract class BaseTest {
+
+    private WebDriver driver;
+
+    @BeforeMethod
+    protected void beforeMethod() {
+        driver = BaseUtils.createDriver();
+    }
+
+    @AfterMethod
+    protected void afterMethod() {
+        driver.quit();
+    }
+
+    protected WebDriver getDriver() {
+        return driver;
+    }
 }
